dd40744ed2b11fa8959ecc0799d406457a27a2d3
chomado <chomado@gmail.com>
2018-01-14 17:12:02 +0900

    プレイヤーを操作できるように

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    public float Speed = 8f;
+    public float MovableRange = 5.5f;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +14,16 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        // プレイヤーを動かす処理
+        this.transform.Translate(
+            x: Input.GetAxisRaw(axisName: "Horizontal") * Speed * Time.deltaTime, 
+            y: 0, 
+            z: 0
+        );
+        // 移動範囲を制限する処理
+        this.transform.position = new Vector2(
+            x: Mathf.Clamp(value: this.transform.position.x, min: -MovableRange, max: MovableRange),
+            y: this.transform.position.y // y 軸についてはとくに制限をかけない
+        );
 	}
 }
