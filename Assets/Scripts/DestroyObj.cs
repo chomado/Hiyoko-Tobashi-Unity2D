3c99ce67a7a136539286bef83aad5b0342a81945
chomado <chomado@gmail.com>
2018-01-15 14:41:28 +0900

    しばらくしたら砲丸が消えるようにした

---
+++ Assets/Scripts/DestroyObj.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObj : MonoBehaviour {
+
+    public float deleteTime = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+        Destroy(obj: gameObject, t: deleteTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
