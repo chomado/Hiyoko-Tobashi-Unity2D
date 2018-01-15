901e30a695885bcd5b1db32c0b312c045a8057a6
chomado <chomado@gmail.com>
2018-01-15 15:51:09 +0900

    ひよこ玉がある地点から無限に湧いてくるようになった

---
+++ Assets/Scripts/HiyokoGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HiyokoGenerator : MonoBehaviour {
+
+    public GameObject hiyokoObj; // ひよこ玉のプレハブ
+    public float interval = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+        // 一定間隔で生成する
+        InvokeRepeating(methodName: "SpawnObj", time: 0.1f, repeatRate: this.interval);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // ひよこ玉を生成する
+    void SpawnObj()
+    {
+        Instantiate(original: this.hiyokoObj, position: transform.position, rotation: transform.rotation);
+    }
+}
